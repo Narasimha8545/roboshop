5f1740fc334c2d7283d7c999a1ceca65299e5fe5
Narasimha Raju <Narasimha8545@gmail.com>
2026-07-30 23:00:47 +0530

    robo

--- mongodb.sh
+++ mongodb.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
+
 R="\e[31m"
 G="\e[32m"
 B="\e[34m"
-N="\e[0m" #normal color
+N="\e[0m"
+
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename "$0" .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOLDER
-echo "script started excuting at: $(date)" |tee -a $LOG_FILE
-#check the user is root or not
-userid=$(id -u)
-if [ $userid -ne 0 ]
-then 
-    echo -e " $R please run the script as root user $N " |tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+mkdir -p "$LOGS_FOLDER"
+
+echo "Script started executing at: $(date)" | tee -a "$LOG_FILE"
+
+# Check whether the user is root
+USER_ID=$(id -u)
+
+if [ "$USER_ID" -ne 0 ]
+then
+    echo -e "${R}Please run the script as root user${N}" | tee -a "$LOG_FILE"
+    exit 1
 else
-    echo -e " $G your are running the root user $N " |tee -a $LOG_FILE
+    echo -e "${G}You are running as root user${N}" | tee -a "$LOG_FILE"
+fi
+
+# Function to validate command execution
+validate() {
+    if [ "$1" -eq 0 ]
+    then
+        echo -e "${G}$2 ... SUCCESS${N}" | tee -a "$LOG_FILE"
+    else
+        echo -e "${R}$2 ... FAILED${N}" | tee -a "$LOG_FILE"
+        exit 1
     fi
-    # validate function takes the exist status, what command they tried to install
-    validate() {
-        if [ $1 -eq 0 ] 
-        then
-            echo -e " $G  $2 is .... successful $N" |tee -a $LOG_FILE
-        else    
-            echo -e " $R  $2 is .... failed $N "|tee -a $LOG_FILE
-            exit 1
-        fi 
-    }
+}
 
-    cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>"$LOG_FILE"
+validate $? "Copy mongodb.repo"
 
-    validate $? "mongo.repo"
+dnf install mongodb-org -y &>>"$LOG_FILE"
+validate $? "Install mongodb-org"
 
-    dnf install mongodb-org -y &>>$LOG_FILE
+systemctl enable mongod &>>"$LOG_FILE"
+validate $? "Enable mongod"
 
-    validate $? "installing mongodb-org"
+systemctl start mongod &>>"$LOG_FILE"
+validate $? "Start mongod"
 
-    systemctl enable mongod &>>$LOG_FILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>"$LOG_FILE"
+validate $? "Update mongod.conf"
 
-    validate $? "enable mongodb"
-
-    systemctl start mongod &>>$LOG_FILE
-
-    validate $? "start mongodb"
-
-    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
-
-    validate $? "update mongod.conf"
-
-    systemctl restart mongod &>>$LOG_FILE
-
-    validate $? "restart mongodb"
+systemctl restart mongod &>>"$LOG_FILE"
+validate $? "Restart mongod"
