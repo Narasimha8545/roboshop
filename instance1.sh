ec099488b435bf383ab5d53be5268a8db9eed8c5
Narasimha Raju <Narasimha8545@gmail.com>
2026-07-30 12:19:26 +0530

    robo

--- instance1.sh
+++ instance1.sh
@@ -2,12 +2,16 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-00e84a02e682ac879"
+
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+
 ZONE_ID="Z02640321D44PD6W59JI5"
 DOMAIN_NAME="natureaws-84.shop"
 
 for instance in "${INSTANCES[@]}"
 do
+
+echo "Creating $instance instance..."
 
 if [ "$instance" == "frontend" ]
 then
@@ -15,6 +19,7 @@ then
 else
     PUBLIC_IP=""
 fi
+
 
 INSTANCE_ID=$(aws ec2 run-instances \
     --image-id "$AMI_ID" \
@@ -25,22 +30,30 @@ INSTANCE_ID=$(aws ec2 run-instances \
     --query "Instances[*].InstanceId" \
     --output text)
 
-echo "$instance instance created with ID: $INSTANCE_ID"
+
+echo "$instance instance ID is $INSTANCE_ID"
+
 
 sleep 10
 
+
 if [ "$instance" != "frontend" ]
 then
+
     IP=$(aws ec2 describe-instances \
         --instance-ids "$INSTANCE_ID" \
         --query "Reservations[*].Instances[*].PrivateIpAddress" \
         --output text)
+
 else
+
     IP=$(aws ec2 describe-instances \
         --instance-ids "$INSTANCE_ID" \
         --query "Reservations[*].Instances[*].PublicIpAddress" \
         --output text)
+
 fi
+
 
 echo "$instance IP address is $IP"
 
