a3683ed52f2daedd01cca93bd65c4fb69cc64a45
Narasimha Raju <Narasimha8545@gmail.com>
2026-07-30 12:07:53 +0530

    robo

--- instance.sh
+++ instance.sh
@@ -11,15 +11,9 @@ do
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-00e84a02e682ac879 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[*].instanceid" --output text)
 if [ $instance != "frontend" ]
 then
-     IP=$(aws ec2 describe-instances \
-      --instance-ids $INSTANCE_ID \
-      --query "Reservations[*].Instances[*].PrivateIpAddress" \
-      --output text)
+     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)
 else
-     IP=$(aws ec2 describe-instances \
-      --instance-ids $INSTANCE_ID \
-      --query "Reservations[*].Instances[*].PublicIpAddress" \
-      --output text)
-fi
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+
 echo "$instance ip address is $IP"
 done 
