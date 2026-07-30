9e030c938ea1fe249ce028912404fe0e78686a3a
Narasimha Raju <Narasimha8545@gmail.com>
2026-07-30 12:16:30 +0530

    robo

---
+++ instance1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-00e84a02e682ac879"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z02640321D44PD6W59JI5"
+DOMAIN_NAME="natureaws-84.shop"
+
+for instance in "${INSTANCES[@]}"
+do
+
+if [ "$instance" == "frontend" ]
+then
+    PUBLIC_IP="--associate-public-ip-address"
+else
+    PUBLIC_IP=""
+fi
+
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type t3.micro \
+    --security-group-ids "$SG_ID" \
+    $PUBLIC_IP \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query "Instances[*].InstanceId" \
+    --output text)
+
+echo "$instance instance created with ID: $INSTANCE_ID"
+
+sleep 10
+
+if [ "$instance" != "frontend" ]
+then
+    IP=$(aws ec2 describe-instances \
+        --instance-ids "$INSTANCE_ID" \
+        --query "Reservations[*].Instances[*].PrivateIpAddress" \
+        --output text)
+else
+    IP=$(aws ec2 describe-instances \
+        --instance-ids "$INSTANCE_ID" \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
+        --output text)
+fi
+
+echo "$instance IP address is $IP"
+
+done
