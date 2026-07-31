b59d90627b0ac2f48af52f4d9ba84a8db1f84f7b
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-01 00:14:10 +0530

    mn

--- frontend.sh
+++ frontend.sh
@@ -5,27 +5,27 @@ G="\e[32m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
+SCRIPT_NAME=$( basename $0 |cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+DOMAIN="mongodb.natureaws-84.shop"
 
-mkdir -p "$LOGS_FOLDER"
+mkdir -p $LOGS_FOLDER
 
 userid=$(id -u)
-
-if [ "$userid" -ne 0 ]
-then
-    echo -e "${R}Please run the script as root user${N}" | tee -a "$LOG_FILE"
-    exit 1
+if [ $userid -ne 0 ]
+then 
+     echo -e " $R please run the script as root user $N " | tee -a $LOG_FILE
+     exit 1 #give other than 0 upto 127
 else
-    echo -e "${G}Running as root user${N}" | tee -a "$LOG_FILE"
+    echo -e " $G your are running the root user $N " | tee -a $LOG_FILE
 fi
-
 validate() {
-    if [ "$1" -eq 0 ]
+    if [ $1 -eq 0 ]
     then
-        echo -e "${G}$2 ... SUCCESS${N}" | tee -a "$LOG_FILE"
-    else
-        echo -e "${R}$2 ... FAILED${N}" | tee -a "$LOG_FILE"
+        echo -e " $N $2 is .... $G successful $N" | tee -a $LOG_FILE
+    else    
+        echo -e " $N $2 is .... $R failed $N "| tee -a $LOG_FILE
         exit 1
     fi
 }
