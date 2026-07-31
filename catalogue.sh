0c1a3c4053c7b513f27c572efa04d5453f6f5ab4
Narasimha Raju <Narasimha8545@gmail.com>
2026-07-31 14:05:19 +0530

    mn

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$($0 |cut -d "." -f1)
+SCRIPT_NAME=$( basename $0 |cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 DOMAIN="mongodb.natureaws-84.shop"
