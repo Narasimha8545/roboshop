6f77ed6297788b977535592bf68d816bafb13319
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-02 20:07:27 +0530

    robo

--- frontend.sh
+++ frontend.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+source ./common.sh
 
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$( basename $0 |cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-DOMAIN="mongodb.natureaws-84.shop"
+check_root
 
-mkdir -p $LOGS_FOLDER
-
-userid=$(id -u)
-if [ $userid -ne 0 ]
-then 
-     echo -e " $R please run the script as root user $N " | tee -a $LOG_FILE
-     exit 1 #give other than 0 upto 127
-else
-    echo -e " $G your are running the root user $N " | tee -a $LOG_FILE
-fi
-validate() {
-    if [ $1 -eq 0 ]
-    then
-        echo -e " $N $2 is .... $G successful $N" | tee -a $LOG_FILE
-    else    
-        echo -e " $N $2 is .... $R failed $N "| tee -a $LOG_FILE
-        exit 1
-    fi
-}
 
 dnf module disable nginx -y &>>"$LOG_FILE"
 validate $? "Disable default nginx module"
@@ -145,4 +120,6 @@ validate $? "Validate nginx configuration"
 systemctl restart nginx &>>"$LOG_FILE"
 validate $? "Restart nginx"
 
-echo -e "${G}Frontend setup completed successfully.${N}"
+print_time
+
+echo -e "$G Frontend setup completed successfully. Time taken: $TOTAL_TIME seconds $N" | tee -a "$LOG_FILE"
