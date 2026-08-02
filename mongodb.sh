f22f43ec3cdb2c1ef6509842572afca6d31abde0
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-02 15:11:43 +0530

    robo

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
 
+# Get the script directory and source common.sh
 source ./common.sh
+
 app_name="mongodb"
 
+# Check if the script is run as root
 check_root
 
- cat >/etc/yum.repos.d/mongo.repo <<EOF
+# Create MongoDB repository
+cat >/etc/yum.repos.d/mongo.repo <<EOF
 [mongodb-org-7.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/7.0/x86_64/
@@ -13,21 +18,28 @@ enabled=1
 gpgkey=https://pgp.mongodb.com/server-7.0.asc
 EOF
 
-validate $? "Creating mongo.repo"
+validate $? "Creating MongoDB repository"
 
+# Install MongoDB
 dnf install -y mongodb-org &>>"$LOG_FILE"
 validate $? "Installing MongoDB"
 
+# Enable MongoDB service
 systemctl enable mongod &>>"$LOG_FILE"
-validate $? "Enable mongod"
+validate $? "Enabling mongod service"
 
+# Start MongoDB service
 systemctl start mongod &>>"$LOG_FILE"
-validate $? "Start mongod"
+validate $? "Starting mongod service"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>"$LOG_FILE"
-validate $? "Update mongod.conf"
+# Allow remote connections
+sed -i 's/^  bindIp: 127.0.0.1/  bindIp: 0.0.0.0/' /etc/mongod.conf &>>"$LOG_FILE"
+validate $? "Updating mongod.conf"
 
+# Restart MongoDB
 systemctl restart mongod &>>"$LOG_FILE"
-validate $? "Restart mongod"
+validate $? "Restarting mongod"
 
-print_time 
+print_time
+
+echo "MongoDB installation completed successfully."
