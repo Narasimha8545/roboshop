ba2f73f926da1b0f9ea9ae4715a63ee3ba6bc0c9
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-03 21:54:51 +0530

    r

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14}    # Default is 14 days if not provided
+DAYS=${3:-14}
 
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
@@ -20,8 +20,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 USERID=$(id -u)
 
 check_root() {
-    if [ "$USERID" -ne 0 ]
-    then
+    if [ "$USERID" -ne 0 ]; then
         echo -e "${R}Please run the script as root user${N}" | tee -a "$LOG_FILE"
         exit 1
     else
@@ -30,8 +29,7 @@ check_root() {
 }
 
 validate() {
-    if [ "$1" -eq 0 ]
-    then
+    if [ "$1" -eq 0 ]; then
         echo -e "${G}$2 ... Successful${N}" | tee -a "$LOG_FILE"
     else
         echo -e "${R}$2 ... Failed${N}" | tee -a "$LOG_FILE"
@@ -40,47 +38,49 @@ validate() {
 }
 
 usage() {
-    echo -e "${R}USAGE:${N} sh backup.sh <source_directory> <destination_directory> <days(optional)>"
+    echo "Usage: sudo sh backup.sh <source_directory> <destination_directory> [days]"
+    exit 1
 }
 
 # Check root user
 check_root
 
 # Check arguments
-if [ $# -lt 2 ]
-then
+if [ $# -lt 2 ]; then
     usage
-    exit 1
 fi
 
 # Check source directory
-if [ ! -d "$SOURCE_DIR" ]
-then
+if [ ! -d "$SOURCE_DIR" ]; then
     echo -e "${R}Source directory '$SOURCE_DIR' does not exist.${N}" | tee -a "$LOG_FILE"
     exit 1
 fi
 
 # Check destination directory
-if [ ! -d "$DEST_DIR" ]
-then
+if [ ! -d "$DEST_DIR" ]; then
     echo -e "${R}Destination directory '$DEST_DIR' does not exist.${N}" | tee -a "$LOG_FILE"
     exit 1
 fi
 
-# Find log files older than DAYS
-FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +$DAYS)
+# Find old log files
+FILES=$(find "$SOURCE_DIR" -type f -name "*.log" -mtime +$DAYS)
 
-
-if [ ! -z "$FILES" ]
-then
-    echo -e "${G}Found the following log files older than $DAYS days:${N}" | tee -a "$LOG_FILE"
+if [ -n "$FILES" ]; then
+    echo -e "${G}Found log files older than $DAYS days:${N}" | tee -a "$LOG_FILE"
     echo "$FILES" | tee -a "$LOG_FILE"
+
     TIMESTAMP=$(date +%Y%m%d%H%M%S)
     ZIPFILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    echo $FILES | zip  "$ZIPFILE" -@ &>>"$LOG_FILE"
-    validate $? "Creating zip file $ZIPFILE"
+
+    echo "$FILES" | zip -@ "$ZIPFILE" &>>"$LOG_FILE"
+    validate $? "Creating zip file"
 
 else
-    echo -e "${G}No log files older than $DAYS days found in '$SOURCE_DIR'.${N}" | tee -a "$LOG_FILE"
+    echo -e "${R}No log files older than $DAYS days found.${N}" | tee -a "$LOG_FILE"
+    exit 1
 fi
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$((END_TIME - START_TIME))
+
+echo -e "${G}Backup completed in ${TOTAL_TIME} seconds.${N}" | tee -a "$LOG_FILE"
