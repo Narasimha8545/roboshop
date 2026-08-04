1462d528f66a68a030c7cfa55cf87cff329835c6
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-04 20:50:19 +0530

    dd

---
+++ disk-usage.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+DISK_USAGE=$(df -HT | awk 'NR>1 {gsub ("%","",$6); print $7,$6}')
+DISK_THRESHOLD=1
+
+while IFS= read -r line
+do 
+  DISK_PATH=$(echo $line | awk '{print $1}')
+  DISK_USAGE_PERCENT=$(echo $line | awk '{print $2}')
+
+  if [ $DISK_USAGE_PERCENT -gt $DISK_THRESHOLD ]; then
+    echo "Warning: $DISK_PATH is using $DISK_USAGE_PERCENT% of its space."
+  fi
+
+done <<<$DISK_USAGE
