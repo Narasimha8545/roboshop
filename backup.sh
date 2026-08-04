af4b4628bfc5e37fc899d5a8c1f7421db2f2b930
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-04 11:23:18 +0530

    robo

--- backup.sh
+++ backup.sh
@@ -78,7 +78,7 @@ then
     echo "$FILES" | tee -a "$LOG_FILE"
     TIMESTAMP=$(date +%Y%m%d)
     ZIPFILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    find "$SOURCE_DIR" -type f -name "*.log" -mtime +"$DAYS" | zip -@ "$ZIPFILE"  &>>"$LOG_FILE"
+    echo "$FILES" | zip -@ "$ZIPFILE"  &>>"$LOG_FILE"
     validate $? "Creating zip file"
 if [ -f "$ZIPFILE" ]
 then
