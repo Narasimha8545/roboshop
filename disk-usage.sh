b7b7c4245a0c5e847171da54d577883fdf1cadb0
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-04 21:05:14 +0530

    dd

--- disk-usage.sh
+++ disk-usage.sh
@@ -7,7 +7,7 @@ MSG=""
 while IFS= read -r line
 do 
   DISK_PATH=$(echo $line | awk '{print $1}')
-  DISK_USAGE_PERCENT=$(echo $line | awk  {gsub ("%"," ",$2); print $2})
+  DISK_USAGE_PERCENT=$(echo $line | awk  '{print $2}' )
 
   if [ $DISK_USAGE_PERCENT -gt $DISK_THRESHOLD ]
    then
