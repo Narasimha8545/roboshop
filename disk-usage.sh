7692f2b7b28ce7b45f88d97eb72f80473b492f30
Narasimha Raju <Narasimha8545@gmail.com>
2026-08-04 20:54:14 +0530

    dd

--- disk-usage.sh
+++ disk-usage.sh
@@ -9,8 +9,10 @@ do
   DISK_PATH=$(echo $line | awk '{print $1}')
   DISK_USAGE_PERCENT=$(echo $line | awk '{print $2}')
 
-  if [ $DISK_USAGE_PERCENT -gt $DISK_THRESHOLD ]; then
-    echo "Warning: $DISK_PATH is using $DISK_USAGE_PERCENT% of its space."
+  if [ $DISK_USAGE_PERCENT -gt $DISK_THRESHOLD ]
+   then
+    MSG= "Warning: $DISK_PATH is using $DISK_USAGE_PERCENT% of its space."
   fi
-
 done <<<$DISK_USAGE
+
+echo "$MSG"
